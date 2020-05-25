0aec187330cf02b3e83e3e58d4ce28d21b65e38c
Hung-Ju <44461359+Hung-Ju@users.noreply.github.com>
2020-05-26 01:33:51 +0800

    資源管理區資料抓取
    資源管理區資料抓取

--- public/javascripts/resource.js
+++ public/javascripts/resource.js
@@ -1,3 +1,27 @@
+//個人資源表格的初始化設定
+function personalResourceTable(){
+    var allPersonalFileData = document.getElementById("allPersonalFileData").value;
+    var allPersonalFile = JSON.parse(allPersonalFileData);
+    var $allPersonalFileDataTable = $('#allPersonalFileDataTable');
+    //console.log(allGroupsFile);
+    
+    $allPersonalFileDataTable.bootstrapTable({
+        columns: [
+            {title: '資源名稱', field: 'file_name', sortable: true},
+            {title: '上傳時間', field: 'file_upload_time', sortable: true},
+            {title: '類型', field: 'file_type', sortable: true},
+            
+            // {title: '', field: 'project_data_id',events: 'window.operateEvents' , formatter: 'researchPurposesButton',width:100}
+            ],
+        theadClasses: 'thead-light',
+        classes: 'table table-bordered',
+        pagination: true,
+        search: true,
+        
+    });
+    $allPersonalFileDataTable.bootstrapTable('load',allPersonalFile);
+}
+
 //小組資源表格的初始化設定
 function groupsResourceTable(){
     var allGroupsFileData = document.getElementById("allGroupsFileData").value;
@@ -8,9 +32,10 @@ function groupsResourceTable(){
     $allGroupsFileDataTable.bootstrapTable({
         columns: [
             {title: '資源名稱', field: 'file_name', sortable: true},
-            {title: '類型', field: 'file_type', sortable: true},
             {title: '上傳時間', field: 'file_upload_time', sortable: true},
             {title: '上傳者', field: 'member_name', sortable: true},
+            {title: '類型', field: 'file_type', sortable: true},
+            
             // {title: '', field: 'project_data_id',events: 'window.operateEvents' , formatter: 'researchPurposesButton',width:100}
             ],
         theadClasses: 'thead-light',
@@ -22,7 +47,10 @@ function groupsResourceTable(){
     $allGroupsFileDataTable.bootstrapTable('load',allGroupsFile);
 }
 
+
+
 $(function(){
+    personalResourceTable();
     groupsResourceTable();
 
 
--- routes/resource.js
+++ routes/resource.js
@@ -9,6 +9,7 @@ router.get('/:gid',function(req, res, next) {
     var member_name = req.session.member_name;
     var groups_name;
     var allGroupsFileArray = [];
+    var allPersonalFileArray = [];
     if(!member_id){
         res.redirect('/');
     }
@@ -32,9 +33,23 @@ router.get('/:gid',function(req, res, next) {
                     allGroupsFileArray.push(allGroupsFileData);
                 }
             }
-            console.log(allGroupsFileArray);
-            
-            res.render('resource',{ title: 'Science Fair科學探究專題系統',gid:gid, member_id:member_id, member_name:member_name, groups_name:groups_name, allGroupsFileData:allGroupsFileArray});
+            return resource.selectPersonalFile(gid, member_id)
+        })
+        .then(function(personalFileData){
+            if(personalFileData){
+                for (var j = 0; j < personalFileData.length; j++){
+                    var file_id = personalFileData[j].file_id;
+                    var node_id_node = personalFileData[j].node_id_node;
+                    var file_name = personalFileData[j].file_name;
+                    var file_type = personalFileData[j].file_type;
+                    var file_upload_time = personalFileData[j].file_upload_time;
+                    var member_name = personalFileData[j].member_name;
+                    allPersonalFileData = {file_id:file_id, node_id_node:node_id_node, file_name:file_name, file_type:file_type, file_upload_time:file_upload_time, member_name:member_name}
+                    allPersonalFileArray.push(allPersonalFileData);
+                }
+            }
+
+            res.render('resource',{ title: 'Science Fair科學探究專題系統',gid:gid, member_id:member_id, member_name:member_name, groups_name:groups_name, allGroupsFileData:allGroupsFileArray, allPersonalFileData:allPersonalFileArray});
         })
         
     }
